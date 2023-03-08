42f89c3fdd9ac88e15924b77aba984509c61f118
Otavio Vasconcelos <otaviocavasc2@gmail.com>
2023-03-08 17:06:26 -0300

    stage merge

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,41 @@
 class PetsController < ApplicationController
+  before_action :set_family, only: %i[new show]
+
+  def index
+    @pets = Pet.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def pet_params
+
+  end
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
 end
