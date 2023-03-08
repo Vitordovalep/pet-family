28bc945950493fd617001995755f407e3834bb6c
Bruno Lauck <brunolauck@gmail.com>
2023-03-08 17:00:50 -0300

    added seeds to db

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,5 +2,7 @@ class Pet < ApplicationRecord
   belongs_to :family
   has_many :users, through: :family
 
+  SPECIES = %w[dog cat rabbit bird]
   validates :name, presence: true
+  validates :species, inclusion: { in: SPECIES }
 end
