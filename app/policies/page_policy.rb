f87b66b30ef996bce316a12dd1da8071abc05778
Araceli Martinez <araceli_martinez94@hotmail.com>
2023-03-13 17:04:10 -0300

    Implemented the admin authorizations ans blazer gem

--- app/policies/page_policy.rb
+++ app/policies/page_policy.rb
@@ -2,12 +2,12 @@ class PagePolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.all
+      scope.all if user.admin?
     end
   end
 
   def show?
-    record.users.include?(user)
+    record.users.include?(user) || user.admin?
   end
-  
+
 end
