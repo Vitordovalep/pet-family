6a4cac5b73d36748a508718cdca7bf19f5abb83c
Bruno Lauck <brunolauck@gmail.com>
2023-03-13 10:50:33 -0300

    Fixed breed problems when creating pets

--- app/javascript/controllers/list_breeds_controller.js
+++ app/javascript/controllers/list_breeds_controller.js
@@ -4,10 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["species", "breeds"]
 
-  connect() {
-    console.log("ouvindo")
-  }
-
   listContent() {
     // console.log(this.speciesTarget.options[this.speciesTarget.selectedIndex].value);
     const id = this.speciesTarget.options[this.speciesTarget.selectedIndex].value
@@ -20,7 +16,7 @@ export default class extends Controller {
 
       data.forEach((breed) => {
         const option = document.createElement("option");
-        option.value = breed[0];
+        option.value = breed[0];  
         option.text = breed[1];
 
         this.breedsTarget.add(option, null);
