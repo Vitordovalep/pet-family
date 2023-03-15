b47f0517524a7b1642a4bcb11b2f436cccc3aab1
Araceli Martinez <araceli_martinez94@hotmail.com>
2023-03-15 14:08:42 -0300

    Stage merge

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ApplicationRecord
   has_one :user, through: :schedule
   accepts_nested_attributes_for :schedule
 
-  TASK_CATEGORIES = ["Banho e tosa", "Medicamento", "Consulta", "Exame", "Comida", "Passeio", "Outros"]
+  TASK_CATEGORIES = ["Banho e tosa", "Comida", "Consulta", "Exame", "Medicamento", "Passeio", "Outros"]
   validates :category, presence: true
   validates :category, inclusion: { in: TASK_CATEGORIES }
 end
