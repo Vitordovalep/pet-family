8a2d1e58ba6aef579f6c9589bc7e6003091e295b
Vitor Pereira <vitordovalep@gmail.com>
2023-03-17 13:04:30 -0300

    landing-page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
-    redirect_to(root_path)
+    redirect_to(main_page_path)
   end
 
   private
