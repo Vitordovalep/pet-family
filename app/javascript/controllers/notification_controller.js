18ddc804297aa1406ab74f44cc74ce2de976ef97
Araceli Martinez <araceli_martinez94@hotmail.com>
2023-03-20 14:43:59 -0300

    fix display of notifications

--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -1,31 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
-import { createConsumer } from "@rails/actioncable"
+
 
 // Connects to data-controller="notification"
 export default class extends Controller {
-static values = {id: Number}
-static targets = ["message"]
+
 
   connect() {
-    console.log("oie")
-    console.log(this.messageTarget)
-    console.log(this.idValue)
-    this.channel= createConsumer().subscriptions.create(
-      {
-        channel: "NotificationsChannel",
-        id: this.idValue
-      },
-      {
-        received: data=> {
-          console.log(data)
-          alert(data.message)
-        }
-      }
-    )
+    console.log("conectado")
   }
 
-  notify() {
-    console.log("notify")
-
+  clear() {
+    this.element.remove()
   }
 }
