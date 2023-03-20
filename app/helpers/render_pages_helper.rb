c7f2c1fdc852e3edc90df1b37db539de030bcc4c
Otavio Vasconcelos <otaviocavasc2@gmail.com>
2023-03-20 14:02:24 -0300

    change conditional order

--- app/helpers/render_pages_helper.rb
+++ app/helpers/render_pages_helper.rb
@@ -4,6 +4,6 @@ module RenderPagesHelper
   end
 
   def show_notifications?
-    (current_page?(main_page_path) || current_page?(family_path(current_user.family)) || current_page?(documents_path)) && user_signed_in?
+    user_signed_in? && (current_page?(main_page_path) || current_page?(family_path(current_user.family)) || current_page?(documents_path))
   end
 end
